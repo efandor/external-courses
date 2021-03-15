b6ac5155d677e7ed78268f214be3282317f8408c
efandor <firstdir@gmail.com>
2021-03-15 20:28:18 +0300

    fix: add solution for task-03 whitout trim() method

--- src/ex4_js-objects-part2/task-03.js
+++ src/ex4_js-objects-part2/task-03.js
@@ -1,5 +1,9 @@
 function deleteFrameSpaces(string) {
-    return string.trim();
+    if ((string[0] === ' ') && (string[string.length - 1] === ' ')) {
+        return string.slice(1, -1);
+    }
+    
+    return string;
 };
 
 module.exports = deleteFrameSpaces;
