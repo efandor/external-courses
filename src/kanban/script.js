56e52551cb76ff1c824c2774dea819621c03739f
efandor <firstdir@gmail.com>
2021-04-23 13:59:49 +0300

    feat: add task from template, add css styles to scroll and other elemants

--- src/kanban/script.js
+++ src/kanban/script.js
@@ -11,23 +11,95 @@ const blockTemplate = `
         </div>
     </div>
 `;
-
+const dataMock = [
+    {
+        title: 'backlog',
+        issues: [
+            {
+                id: 'task1',
+                name: 'Sprint bugfix11 111'
+            },
+            {
+                id: 'task2',
+                name: 'Sprint bugfix222 22'
+            },
+            {
+                id: 'task3',
+                name: 'Sprint bugfix333333 3333333 33333 33333'
+            }
+        ],
+    },
+    {
+        title: 'ready',
+        issues: [
+            {
+                id: 'task2',
+                name: 'Sprint bugfix222 22'
+            }
+    ],
+    },
+    {
+        title: 'progress',
+        issues: [
+            {
+                id: 'task3',
+                name: 'Sprint bugfix33 333'
+            }
+        ],
+    },
+    {
+        title: 'finished',
+        issues: [
+            {
+                id: 'task4',
+                name: 'Sprint bugfix444 44'
+            }
+        ],
+    },
+];
+const cards = JSON.parse(localStorage.getItem('cards')) || dataMock;
+const backlog = document.querySelector('.block-container-backlog');
+const ready = document.querySelector('.block-container-ready');
+const progress = document.querySelector('.block-container-progress');
+const finished = document.querySelector('.block-container-finished');
 const menuArrow = document.querySelector('.menu-closed');
 const avatar = document.querySelector('.avatar');
 const dropDownMenu = document.createElement('div');
+
 dropDownMenu.className = 'dropDownMenu';
 dropDownMenu.innerHTML = blockTemplate;
 
-const toggleMenu = () => {
+cards.forEach(elem => {
+    elem.issues.forEach(card => appendTask(card, backlog, ready, progress, finished, elem.title));
+})
+
+function appendTask(cardElem, backlogElem, readyElem, progressElem, finishedElem, blockName) {
+    const task = document.createElement('div');
+    task.className = 'task';
+    task.innerHTML = cardElem.name;
+
+    switch(blockName) {
+        case 'backlog':
+            backlogElem.append(task);
+            break;
+        case 'ready':
+            readyElem.append(task);
+            break;
+        case 'progress':
+            progressElem.append(task);
+            break;
+        case 'finished':
+            finishedElem.append(task);
+            break;
+        default:
+            break;
+      }
+}
+
+function toggleMenu() {
     menuArrow.className = (menuArrow.className === 'menu-opened') ? 'menu-closed' : 'menu-opened';
-
-    if (menuArrow.className === 'menu-opened') {
-        avatar.appendChild(dropDownMenu);
-    }
-
-    if (menuArrow.className === 'menu-closed') {
-        dropDownMenu.remove();
-    }
+    if (menuArrow.className === 'menu-opened') avatar.appendChild(dropDownMenu);
+    if (menuArrow.className === 'menu-closed') dropDownMenu.remove();
 };
 
 menuArrow.addEventListener('click', toggleMenu);
