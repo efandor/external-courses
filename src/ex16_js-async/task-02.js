973cc5df69879134d9f0e8c8f63e1bc67758e7ae
efandor <firstdir@gmail.com>
2021-05-19 17:20:19 +0300

    feat: add custom delay settings

--- src/ex16_js-async/task-02.js
+++ src/ex16_js-async/task-02.js
@@ -1,15 +1,16 @@
 const data = document.getElementById('data');
 const output = document.getElementById('output');
+const delay = document.getElementById('delay');
 
 /* eslint no-return-assign: 0, no-param-reassign: 0*/
-const debounce = (outputElem, inputElem) => {
+const debounce = (outputElem, inputElem, delayElem) => {
     let timeout;
     const showDelayedOutput = () => {
         clearTimeout(timeout);
-        timeout = setTimeout(() => outputElem.value = inputElem.value, 3000);
+        timeout = setTimeout(() => outputElem.value = inputElem.value, delayElem.value);
     }
 
     return showDelayedOutput;
 }
 
-data.addEventListener("keydown", debounce(output, data));
+data.addEventListener("input", debounce(output, data, delay));
