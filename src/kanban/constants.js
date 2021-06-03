dbf6674cd92a0151183861ef4bf62891731d939b
efandor <firstdir@gmail.com>
2021-06-03 16:37:52 +0300

    feat: add select previous task item

--- src/kanban/constants.js
+++ src/kanban/constants.js
@@ -1,44 +1,48 @@
 export const dataMock = [
     {
         title: 'backlog',
+        blockId: 0,
         issues: [
             {
-                id: 'task1',
+                id: '1',
                 name: 'Sprint bugfix11 111'
             },
             {
-                id: 'task2',
+                id: '2',
                 name: 'Sprint bugfix222 22'
             },
             {
-                id: 'task3',
+                id: '3',
                 name: 'Sprint bugfix333333 3333333 33333 33333'
             }
         ],
     },
     {
         title: 'ready',
+        blockId: 1,
         issues: [
             {
-                id: 'task2',
+                id: '2',
                 name: 'Sprint bugfix222 22'
             }
     ],
     },
     {
         title: 'progress',
+        blockId: 2,
         issues: [
             {
-                id: 'task3',
+                id: '3',
                 name: 'Sprint bugfix33 333'
             }
         ],
     },
     {
         title: 'finished',
+        blockId: 3,
         issues: [
             {
-                id: 'task4',
+                id: '4',
                 name: 'Sprint bugfix444 44'
             }
         ],
--- src/kanban/kanban.js
+++ src/kanban/kanban.js
@@ -3,18 +3,19 @@ import {dataMock} from './constants.js';
 class Kanban {
     constructor() {
         this.cards = JSON.parse(localStorage.getItem('cards')) || dataMock;
-        this.columnNames = ['backlog', 'ready', 'inProgress', 'finished'];
+        this.columnNames = ['backlog', 'ready', 'progress', 'finished'];
         this.columns = {
             backlog : document.querySelector('.block-container-backlog'),
             ready : document.querySelector('.block-container-ready'),
             progress : document.querySelector('.block-container-progress'),
             finished : document.querySelector('.block-container-finished'),
         };
-        this.menuArrow = document.querySelector('.menu-closed');
+        this.menuArrow = document.querySelector('.menu');
         this.avatar = document.querySelector('.avatar');
         this.dropDownMenu = document.createElement('div');
         this.dropDownMenu.className = 'dropDownMenu';
         this.dropDownMenu.innerHTML = blockTemplate;
+        this.cardButtons = [...document.getElementsByClassName('item-menu-button')];
         this.addCardToBacklog = document.querySelector('.item-menu-backlog');
         this.addCardToReady = document.getElementById('ready');
         this.addCardToProgress = document.getElementById('progress');
@@ -57,9 +58,9 @@ class Kanban {
     }
     
     toggleMenu = () => {
-        this.menuArrow.className = (this.menuArrow.className === 'menu-opened') ? 'menu-closed' : 'menu-opened';
-        if (this.menuArrow.className === 'menu-opened') this.avatar.appendChild(this.dropDownMenu);
-        if (this.menuArrow.className === 'menu-closed') this.dropDownMenu.remove();
+        this.menuArrow.classList.toggle('opened');
+        if (this.menuArrow.className === 'menu opened') this.avatar.appendChild(this.dropDownMenu);
+        if (this.menuArrow.className === 'menu') this.dropDownMenu.remove();
     };
     
     addCard = () => {
@@ -72,18 +73,74 @@ class Kanban {
     }
     
     moveCard = (e) => {
-        console.log(this.cards[e.target.id - 1].issues.length);
-        e.target.className = (e.target.className === 'menu-opened') ? 'menu-closed' : 'menu-opened';
-        if (e.target.className === 'menu-opened') e.target.parentElement.appendChild(this.dropDownMenu);
-        if (e.target.className === 'menu-closed') this.dropDownMenu.remove();
+        // e.target.className = (e.target.className === 'opened') ? 'menu' : 'opened';
+        
+        // console.log(dataMock);
+
+        let findedId = this.columnNames.indexOf(e.target.id) - 1;
+        // console.log(findedId);
+        // console.log(taskList);
+
+        console.log(this.cards[findedId].issues);
+
+        let listTemplate = `<form>
+        <p>
+            <select class="taskSelect" name="task">
+            <option disabled selected>Select task...</option>'`;
+
+        this.cards[findedId].issues.forEach(elem => listTemplate += `
+        <option value="${elem.id}">${elem.name}</option>`);
+
+        listTemplate += `
+</select>
+</p>
+        </form>`
+
+
+        // console.log(listTemplate);
+
+        this.prevTasklist = document.createElement('div');
+        this.prevTasklist.className = 'dropDownList';
+        this.prevTasklist.id = `${e.target}`;
+        this.prevTasklist.innerHTML = listTemplate;
+
+        const select = document.querySelector('.dropDownList');
+        // const container = document.getElementsByClassName('taskSelect');
+        // console.log(e.target.parentElement.parentElement.children[1]);
+        console.log(e.target);
+
+
+        
+        const changeCard = (e) => {
+            console.log(e);
+            return e;
+        }
+
+
+        e.target.classList.toggle('disabled');
+        
+        if (e.target.className === 'item-menu-button disabled') e.target.parentElement.parentElement.children[1].append(this.prevTasklist);
+        if (e.target.className === 'item-menu-button') this.prevTasklist.remove();
+        e.target.disabled = true;
+
+        select.addEventListener('click', () => changeCard);
+
+       
     }
+
+    
     
     initHandlers = () => {
         this.menuArrow.addEventListener('click', this.toggleMenu);
         this.addCardToBacklog.addEventListener('click', this.addCard);
-        this.addCardToReady.addEventListener('click', this.moveCard);
-        this.addCardToProgress.addEventListener('click', this.moveCard);
-        this.addCardToFinished.addEventListener('click', this.moveCard);
+        // this.addCardToReady.addEventListener('click', this.moveCard);
+        // this.addCardToProgress.addEventListener('click', this.moveCard);
+        // this.addCardToFinished.addEventListener('click', this.moveCard);
+        this.cardButtons.forEach(elem => elem.addEventListener('click', this.moveCard));
+    }
+
+    saveToLocalStorage = () => {
+        localStorage.setItem('cards', JSON.stringify(this.cards));
     }
 }
 
