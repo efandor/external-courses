f204c85dcdecee4a215be99259a5df651a8ba2d4
efandor <firstdir@gmail.com>
2021-06-03 12:08:11 +0300

    fix: set webpack build mode to production

--- src/kanban/webpack.config.js
+++ src/kanban/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "main_bundle.js",
     },
-   // mode: 'development',
+    mode: 'production',
     devServer: {
         contentBase: path.join(__dirname, "dist"),
         port: 9005,
